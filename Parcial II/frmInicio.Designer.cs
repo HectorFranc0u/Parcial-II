ee0c4899b3df012f6d6fd8bd2a4cbe11cccd604b
Rene Fuentes <fuentesbenitezreneismael@gmail.com>
2021-10-20 16:13:27 -0600

    Avance

--- Parcial II/frmInicio.Designer.cs
+++ Parcial II/frmInicio.Designer.cs
@@ -29,19 +29,69 @@ namespace Parcial_II
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPws = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 482);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.Color.Black;
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUser.ForeColor = System.Drawing.Color.LightGray;
+            this.txtUser.Location = new System.Drawing.Point(441, 122);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(559, 20);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "USUARIO ";
+            // 
+            // txtPws
+            // 
+            this.txtPws.BackColor = System.Drawing.Color.Black;
+            this.txtPws.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPws.ForeColor = System.Drawing.Color.LightGray;
+            this.txtPws.Location = new System.Drawing.Point(441, 226);
+            this.txtPws.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPws.Name = "txtPws";
+            this.txtPws.Size = new System.Drawing.Size(559, 20);
+            this.txtPws.TabIndex = 2;
+            this.txtPws.Text = "USUARIO ";
             // 
             // frmInicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 507);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1170, 482);
+            this.Controls.Add(this.txtPws);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmInicio";
+            this.Opacity = 0.9D;
             this.Text = "InicioDeSesión";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPws;
     }
 }
