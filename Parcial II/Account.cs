435d496a1603591485a70022bdc87b3488938e6d
HectorFranc0u <halejandrofb@gmail.com>
2021-10-20 13:07:08 -0600

    configuracion

---
+++ Parcial II/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Parcial_II
+{
+    class Account
+    {
+        public int _UserID { get; set; }
+        public string _UserFirstname { get; set; }
+        public string _UserLastname { get; set; }
+        public string _UserEmail { get; set; }
+        public string _UserRegisterdate { get; set; }
+        public string _Usname { get; set; }
+        public string _Uspassword { get; set; }
+
+        private Crud crud = new Crud();
+
+        public MySqlDataReader getUsers()
+        {
+            string query = "SELECT codUser,firstname,lastname,email,registerdate,ussername,password FROM account";
+
+            return crud.select(query);
+        }
+
+        public void NewUser()
+        {
+            string query = "INSERT INTO account(firstname, lastname, email, registerdate, ussername, password)" +
+                    "VALUES ('" + _UserFirstname + "', '" + _UserLastname + "', '" + _UserEmail + "', '" + _UserRegisterdate + "', '" + _Usname + "', '" + _Uspassword + "')";
+            crud.executeQuery(query);
+        }
+    }
+}
---
+++ Parcial II/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Parcial_II
+{
+    class Connection
+    {
+        private MySqlConnection conn =
+        new MySqlConnection("Server=localhost;Database=parcial;Uid=root;Pwd=usbw;SSL Mode=None");
+        public MySqlCommand command;
+
+        public MySqlConnection openconnection()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            return conn;
+        }
+
+        public MySqlConnection closeconnection()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            return conn;
+        }
+    }
+}
