ef62353b702da4953ab3cf56c2bbe1f9fdd9e3dc
Rene Fuentes <fuentesbenitezreneismael@gmail.com>
2021-10-20 19:51:36 -0600

    Interfaz de Inicio de Sesion terminada

--- Parcial II/frmRegister.Designer.cs
+++ Parcial II/frmRegister.Designer.cs
@@ -31,13 +31,16 @@ namespace Parcial_II
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtUserFirtsName = new System.Windows.Forms.TextBox();
+            this.txtUserLastName = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtUsname = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtUspassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUserEmail = new System.Windows.Forms.TextBox();
+            this.btnCrearUser = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -45,7 +48,7 @@ namespace Parcial_II
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(18, 13);
+            this.label1.Location = new System.Drawing.Point(18, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(170, 25);
@@ -56,31 +59,31 @@ namespace Parcial_II
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 97);
+            this.label2.Location = new System.Drawing.Point(19, 98);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(77, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre:";
             // 
-            // textBox1
+            // txtUserFirtsName
             // 
-            this.textBox1.Location = new System.Drawing.Point(104, 95);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(190, 27);
-            this.textBox1.TabIndex = 2;
+            this.txtUserFirtsName.Location = new System.Drawing.Point(183, 95);
+            this.txtUserFirtsName.Name = "txtUserFirtsName";
+            this.txtUserFirtsName.Size = new System.Drawing.Size(190, 27);
+            this.txtUserFirtsName.TabIndex = 2;
             // 
-            // textBox2
+            // txtUserLastName
             // 
-            this.textBox2.Location = new System.Drawing.Point(462, 95);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(190, 27);
-            this.textBox2.TabIndex = 3;
+            this.txtUserLastName.Location = new System.Drawing.Point(497, 98);
+            this.txtUserLastName.Name = "txtUserLastName";
+            this.txtUserLastName.Size = new System.Drawing.Size(190, 27);
+            this.txtUserLastName.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(381, 98);
+            this.label3.Location = new System.Drawing.Point(389, 98);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 19);
             this.label3.TabIndex = 4;
@@ -89,55 +92,84 @@ namespace Parcial_II
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 168);
+            this.label4.Location = new System.Drawing.Point(12, 227);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(162, 19);
             this.label4.TabIndex = 5;
             this.label4.Text = "Nombre de Usuario:";
             // 
-            // textBox3
+            // txtUsname
             // 
-            this.textBox3.Location = new System.Drawing.Point(180, 165);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(190, 27);
-            this.textBox3.TabIndex = 6;
+            this.txtUsname.Location = new System.Drawing.Point(183, 224);
+            this.txtUsname.Name = "txtUsname";
+            this.txtUsname.Size = new System.Drawing.Size(190, 27);
+            this.txtUsname.TabIndex = 6;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(381, 168);
+            this.label5.Location = new System.Drawing.Point(389, 227);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(102, 19);
             this.label5.TabIndex = 7;
             this.label5.Text = "Contraseña:";
             // 
-            // textBox4
+            // txtUspassword
             // 
-            this.textBox4.Location = new System.Drawing.Point(490, 165);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(190, 27);
-            this.textBox4.TabIndex = 8;
+            this.txtUspassword.Location = new System.Drawing.Point(497, 224);
+            this.txtUspassword.Name = "txtUspassword";
+            this.txtUspassword.Size = new System.Drawing.Size(190, 27);
+            this.txtUspassword.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(179, 324);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(154, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Correo Electronico:";
+            // 
+            // txtUserEmail
+            // 
+            this.txtUserEmail.Location = new System.Drawing.Point(339, 321);
+            this.txtUserEmail.Name = "txtUserEmail";
+            this.txtUserEmail.Size = new System.Drawing.Size(190, 27);
+            this.txtUserEmail.TabIndex = 10;
+            // 
+            // btnCrearUser
+            // 
+            this.btnCrearUser.Location = new System.Drawing.Point(295, 394);
+            this.btnCrearUser.Name = "btnCrearUser";
+            this.btnCrearUser.Size = new System.Drawing.Size(173, 65);
+            this.btnCrearUser.TabIndex = 11;
+            this.btnCrearUser.Text = "Crear Usuario";
+            this.btnCrearUser.UseVisualStyleBackColor = true;
             // 
             // frmRegister
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 658);
-            this.Controls.Add(this.textBox4);
+            this.ClientSize = new System.Drawing.Size(732, 519);
+            this.Controls.Add(this.btnCrearUser);
+            this.Controls.Add(this.txtUserEmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtUspassword);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtUsname);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtUserLastName);
+            this.Controls.Add(this.txtUserFirtsName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmRegister";
-            this.Text = "frmRegister";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,12 +179,15 @@ namespace Parcial_II
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtUserFirtsName;
+        private System.Windows.Forms.TextBox txtUserLastName;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtUsname;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtUspassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUserEmail;
+        private System.Windows.Forms.Button btnCrearUser;
     }
 }
