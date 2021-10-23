75240a9d49560b484a617ae65eb28a767a1d4520
geomalv02 <smis063721@ugb.edu.sv>
2021-10-23 13:26:10 -0600

    logs

--- Parcial II/frmLogs.cs
+++ Parcial II/frmLogs.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MetroFramework.Forms;
+using MySql.Data.MySqlClient;
+
 namespace Parcial_II
 {
     public partial class frmLogs : Form
@@ -15,6 +17,36 @@ namespace Parcial_II
         public frmLogs()
         {
             InitializeComponent();
+        }
+
+        private void gtdLogs_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void frmLogs_Load(object sender, EventArgs e)
+        {
+            filldgv();
+        }
+
+        private void filldgv()
+        {
+            Account account = new Account();
+
+            gtdLogs.Columns.Add("_UserID", "ID");
+            gtdLogs.Columns.Add("_Usname", "USUARIO");
+            gtdLogs.Columns.Add("_TimeLoggedIn", "TIMEPO EN EL QUE ESTUVO LOGGEADO");
+
+            MySqlDataReader dataReader = account.getlogs();
+
+            while (dataReader.Read())
+            {
+                gtdLogs.Rows.Add(
+                    dataReader.GetValue(0),
+                    dataReader.GetValue(1),
+                    dataReader.GetValue(2)
+                    );
+            }
         }
     }
 }
